fb2ef940c9d364e8491960f5ab645d80b3dfb0c9
Umi252 <umisiguca25@gmail.com>
2023-02-22 07:22:07 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,13 @@
 /**
  * print_alphabet_x10 - prints the alphabet 10 times
  */
-
 void print_alphabet_x10(void)
 {
     char c;
     int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-        {
+    for (i = 0; i < 10; i++) {
+        for (c = 'a'; c <= 'z'; c++) {
             _putchar(c);
         }
         _putchar('\n');
