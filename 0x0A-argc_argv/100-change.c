7a8568af029c93ba6d613b0f13ca9bcba873a995
Umi252 <umisiguca25@gmail.com>
2023-03-10 19:28:34 -0800

    up to date

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int position = 0, total = 0, change = 0, aux = 0;
+    int coins[] = {25, 10, 5, 2, 1};
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    total = atoi(argv[1]);
+
+    if (total <= 0)
+    {
+        printf("0\n");
+        return (0);
+    }
+
+    while (coins[position])
+    {
+        if (total >= coins[position])
+        {
+            aux = (total / coins[position]);
+            change += aux;
+            total -= coins[position] * aux;
+        }
+        position++;
+    }
+
+    printf("%d\n", change);
+    return 0;
+}
+
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int count = 0;
+
+    if (argc > 0) {
+        while (count < argc) {
+            printf("%s\n", argv[count]);
+            count++;
+        }
+    }
+
+    return 0;
+}
