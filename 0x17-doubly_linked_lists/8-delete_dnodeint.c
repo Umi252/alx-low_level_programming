d0f890d23fa4f4fc51d8ff34b14d0bde7916b076
Umi252 <umisiguca25@gmail.com>
2023-08-17 10:58:53 +0000

    Delete at indexs

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,50 +1,52 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at a given index in a dlistint_t list.
- * @head: Pointer to a pointer to the head of the doubly linked list.
- * @index: Index of the node that should be deleted.
- * Return: 1 if succeeded, -1 if failed.
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
+ *
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current, *prev, *next;
-    unsigned int count = 0;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	h1 = *head;
 
-    current = *head;
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
-    if (index == 0)
-    {
-        *head = current->next;
-        if (current->next != NULL)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
+	i = 0;
 
-    while (current != NULL)
-    {
-        if (count == index)
-        {
-            prev = current->prev;
-            next = current->next;
-            if (prev != NULL)
-                prev->next = next;
-            if (next != NULL)
-                next->prev = prev;
-            free(current);
-            return (1);
-        }
-        current = current->next;
-        count++;
-    }
+	while (h1 != NULL)
+	{
+		if (i == index)
+		{
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
 
-    return (-1);
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
+			return (1);
+		}
+		h2 = h1;
+		h1 = h1->next;
+		i++;
+	}
+
+	return (-1);
 }
-
-
